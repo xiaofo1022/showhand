1a27c92937dd825d51f7a3719a1c2e395c4d3a0a
503122660 <503122660@gehealthcare.com>
2026-03-25 17:19:05 +0800

    create the project.

---
+++ src/main/java/net/xiaofo1022/showhand/ShowhandApplication.java
@@ -0,0 +1,13 @@
+package net.xiaofo1022.showhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowhandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowhandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/xiaofo1022/showhand/ShowhandApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xiaofo1022.showhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowhandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
